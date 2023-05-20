2c9416cc3e1a382ece62c636ef8fc5086f96c9e3
wakamezake <hotwater1367@gmail.com>
2023-05-20 13:00:17 +0900

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+use scraper::{Html, Selector};
+use url::Url;
+
+async fn fetch_page(url: &str) -> Result<String, Error> {
+    let text = reqwest::get(url).await?.text().await?;
+    Ok(text)
+}
+
+async fn extract_download_link(page: &str, base: &Url) -> Result<Url, url::ParseError> {
+    let document = Html::parse_document(page);
+    let selector = Selector::parse("a[href]").unwrap();
+    let url = document
+        .select(&selector)
+        .filter_map(|el| el.value().attr("href"))
+        .find(|&href| href.contains("servlet.FileDownload"))
+        .ok_or_else(|| url::ParseError::SetHostOnCannotBeABaseUrl)?;
+    let url = base.join(url)?;
+    Ok(url)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let base_url = Url::parse("https://www.fit-portal.go.jp/PublicInfo")?;
+    let page = fetch_page(base_url.as_str()).await?;
+    let download_link = extract_download_link(&page, &base_url).await?;
+    println!("{}", download_link);
+    Ok(())
 }
